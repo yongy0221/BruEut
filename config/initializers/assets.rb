479492d5d35753f407bd2f6f7016ba078cc56602
greenyim <greenyim@likelion.org>
2017-08-23 22:41:13 -0700

    change login and landing X nav

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,7 +5,6 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( login.css )
 Rails.application.config.assets.precompile += %w( custom.css )
 Rails.application.config.assets.precompile += %w( regis.css )
-Rails.application.config.assets.precompile += %w( menubar.css )
 
 
 
